c0635b869862ae863f8c40e0bbbd8c9fa8664177
André L F S Bacci <alfsb@github.com>
2023-07-09 18:24:52 -0300

    Primeiras estruturas básicas.

--- src/lib/Parser/Page.cs
+++ src/lib/Parser/Page.cs
@@ -1,4 +1,29 @@
+using System.Linq;
+
 namespace DadosCorretora.NotaCorretagem.Parser;
 
 public class Page
-{ }
+{
+    public int Number { get; private set; }
+
+    private Text[] Texts;
+    private bool Sorted = false;
+
+    public Page(int number, IEnumerable<Text> texts)
+    {
+        this.Number = number;
+        this.Texts = texts.ToArray();
+        this.EnsureSorted();
+    }
+
+    private void EnsureSorted()
+    {
+        if (this.Sorted)
+            return;
+        this.Texts = this.Texts
+            .OrderBy(t => t.YMin) // Ordena de cima para baixo
+            .ThenBy(t => t.XMin)  // Da direita para esqueda
+            .ToArray();
+        this.Sorted = true;
+    }
+}
--- src/lib/Parser/Text.cs
+++ src/lib/Parser/Text.cs
@@ -1,4 +1,10 @@
 namespace DadosCorretora.NotaCorretagem.Parser;
 
-public class Text
-{ }
+public record Text
+{
+    public string Content = "";
+    public decimal XMin { get; }
+    public decimal XMax { get; }
+    public decimal YMin { get; }
+    public decimal YMax { get; }
+}
