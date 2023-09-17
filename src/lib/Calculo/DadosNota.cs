6ac106e95be95dd891de3a0a4d706b331d4f2e19
Augusto Arcoverde da Rocha <agarobr@gmail.com>
2023-09-17 19:01:05 -0300

    Dados tipados de operacao e custos

--- src/lib/Calculo/DadosNota.cs
+++ src/lib/Calculo/DadosNota.cs
@@ -1,15 +1,41 @@
+using DadosCorretora.NotaCorretagem.Extracao;
+
 namespace DadosCorretora.NotaCorretagem.Calculo;
 
 public class DadosNota
 {
+    public DadosNota()
+    {
+        Operacoes = new List<Operacao>();
+        Custos = new List<Custo>();
+    }
+
+    public List<Operacao> Operacoes;
+
+    public List<Custo> Custos;
 
     public class Operacao
     {
-        public string NumeroNote = "";
-        public DateTime DataNota;
+        public string NumeroNota = "";
+        public DateOnly DataNota;
         public string CodigoAtivo = "";
         public decimal Quantidade;
         public decimal ValorOperacao;
     }
-    public class Custo;
+    public class Custo {
+        public string NumeroNota = "";
+        public DateOnly DataPregao;
+        public decimal CustoTotal;
+        public decimal IrrfSobreOperacoes;
+    }
+
+    public void AcumulaCusto(DadosNota.Custo custo)
+    {
+        this.Custos.Add(custo);
+    }
+
+    public void AcumulaOperacao(DadosNota.Operacao operacao)
+    {
+        this.Operacoes.Add(operacao);
+    }
 }
