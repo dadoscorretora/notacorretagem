246e9237f98c40d1a0abab9f6500f58ce80fe76c
André L F S Bacci <alfsb@github.com>
2023-09-17 15:43:04 -0300

    Classes iniciais do cálculo.

---
+++ src/lib/Calculo/DadosNota.cs
@@ -0,0 +1,15 @@
+namespace DadosCorretora.NotaCorretagem.Calculo;
+
+public class DadosNota
+{
+
+    public class Operacao
+    {
+        public string NumeroNote = "";
+        public DateTime DataNota;
+        public string CodigoAtivo = "";
+        public decimal Quantidade;
+        public decimal ValorOperacao;
+    }
+    public class Custo;
+}
