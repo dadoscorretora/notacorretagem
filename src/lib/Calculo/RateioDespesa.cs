f060e50c81db74b35c500ef451a83adf68154b0b
André L F S Bacci <alfsb@github.com>
2023-12-16 12:51:04 -0300

    Rateio de despesas pelo valor absoluto, primeiro trunca, depois arredonda, por fim distribui residuais

--- src/lib/Calculo/RateioDespesa.cs
+++ src/lib/Calculo/RateioDespesa.cs
@@ -5,50 +5,53 @@ public class RateioDespesa
     public static void Calcular(DadosNota dadosNota)
     {
         var nrNotas = dadosNota.Custos.GroupBy(c => c.NumeroNota);
-        if (nrNotas.Any( g => g.Count() > 1))
+        if (nrNotas.Any(g => g.Count() > 1))
         {
             throw new Exception("Mais de um custo para a mesma nota. Provável erro na extração");
         }
         dadosNota.Custos.ToList().ForEach(custo =>
         {
-            var operacoes = dadosNota.Operacoes.Where(o => o.NumeroNota == custo.NumeroNota).OrderBy(o => o.ValorOperacao);
-            var totalOperacoes = operacoes.Sum(o => o.ValorOperacao);
-            var custoTotal = custo!.CustoTotal;
-            decimal restoCusto = custoTotal;
-            restoCusto = DistribuiRateio(operacoes, totalOperacoes, custoTotal, restoCusto);
+            var operacoes = dadosNota.Operacoes
+                .Where(o => o.NumeroNota == custo.NumeroNota)
+                .OrderByDescending(o => Math.Abs(o.ValorOperacao));
 
-            if (restoCusto > 0)
+            var restoCusto = DistribuiRateio(operacoes, custo!.CustoTotal, MidpointRounding.ToZero);
+
+            if (restoCusto < 0)
             {
-                throw new Exception("Arrendondamento do rateio maior que o custo");
+                restoCusto = DistribuiRateio(operacoes, restoCusto, MidpointRounding.AwayFromZero);
             }
 
-            custoTotal = restoCusto;
+            for (var opIndex = 0; restoCusto < 0 && opIndex < operacoes.Count(); opIndex++)
+            {
+                var operacao = operacoes.ElementAt(opIndex);
+                operacao.CustoOperacao -= 0.01m;
+                restoCusto += 0.01m;
+            }
 
             if (restoCusto != 0)
             {
-                restoCusto = DistribuiRateio(operacoes, totalOperacoes, restoCusto, restoCusto);
-            }
-
-            var opCount = 0;
-            while (restoCusto != 0) {
-                var operacao = operacoes.ElementAt(opCount);
-                restoCusto += 0.01m;
-                operacao.CustoOperacao -= 0.01m;
-                opCount++;
+                throw new Exception("Falha na rateio de custos. Residual: " + restoCusto);
             }
         });
-
     }
 
-    private static decimal DistribuiRateio(IEnumerable<DadosNota.Operacao> operacoes, decimal totalOperacoes, decimal custoTotal, decimal restoCusto)
+    private static decimal DistribuiRateio(IEnumerable<DadosNota.Operacao> operacoes, decimal custoTotal, MidpointRounding tratamentoFracoes)
     {
+        var restoCusto = custoTotal;
+        var totalOperacoes = operacoes.Sum(o => Math.Abs(o.ValorOperacao));
+
         operacoes.ToList().ForEach(operacao =>
         {
-            decimal percentual = operacao.ValorOperacao / totalOperacoes;
-            var custoRateado = Math.Round(custoTotal * percentual, 2, MidpointRounding.ToZero);
-            operacao.CustoOperacao += custoRateado;
-            restoCusto -= custoRateado;
+            if (restoCusto != 0)
+            {
+                decimal percentual = Math.Abs(operacao.ValorOperacao) / totalOperacoes;
+                var custoRateado = Math.Round(custoTotal * percentual, 2, tratamentoFracoes);
+                operacao.CustoOperacao += custoRateado;
+                restoCusto -= custoRateado;
+            }
         });
+
         return restoCusto;
     }
 }
